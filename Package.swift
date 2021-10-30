e25a12513099c375ad787349232cd05ebe40c7c4
Jan Kubny <30832483+SmartJSONEditor@users.noreply.github.com>
2021-10-30 08:45:58 -0600

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -11,7 +11,7 @@ let package = Package(
         .library(name: "LeafKit", targets: ["LeafKit"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/apple/swift-nio.git", branch: "main"),
+        .package(url: "https://github.com/apple/swift-nio.git", from: "2.2.0"),
     ],
     targets: [
         .target(name: "LeafKit", dependencies: [
